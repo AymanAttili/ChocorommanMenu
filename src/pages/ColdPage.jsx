48ae9a87dcaa20c4fe2efbc38102766ada4b5a98
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2024-12-17 20:39:50 +0200

    ..

--- src/pages/ColdPage.jsx
+++ src/pages/ColdPage.jsx
@@ -25,7 +25,7 @@ function ColdPage() {
         boxShadow: 24,
         p: 4,
     };
-    const openPicture = useCallback((openPicture) => {
+    const openPicture = useCallback((pic) => {
         setCurrentPic(pic)
         setOpenModal(true)
     }, [setCurrentPic, setOpenModal])
