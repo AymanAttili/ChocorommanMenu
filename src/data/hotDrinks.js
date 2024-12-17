9fe62acd14a40f21fe993aa0214cd418294f8dcd
Ayman Attili <a.l.ateeli@students.ptuk.edu.ps>
2024-12-17 21:41:22 +0200

    ...

--- src/data/hotDrinks.js
+++ src/data/hotDrinks.js
@@ -2,45 +2,158 @@ import cappuccino from '../images/cappuccino.png'
 import choco from '../images/choco.png'
 import coffee from '../images/coffee.png'
 import nescafe from '../images/nescafe.png'
-import tea from '../images/tea.png'
+import redTea from '../images/redTea.png'
+import greenTea from '../images/greenTea.png'
+import milk from '../images/milk.png'
+import herbal from '../images/herbal.png'
 export const hotDrinks = [
     {
         id: 1,
         name: 'مشروبات ساخنة',
-        prices:{
-            m:2,
-            l:3
+        prices: {
+            m: 2,
+            l: 3
         },
-        flavours:[
+        flavours: [
             {
                 id: 1,
                 name: 'نسكافيه',
                 image: nescafe,
-                isAvailable: 1
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
             },
             {
                 id: 2,
-                name: 'قهوة عربية',
+                name: 'قهوة',
                 image: coffee,
-                isAvailable: 1
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
             },
             {
                 id: 3,
-                name: 'شاي',
-                image: tea,
-                isAvailable: 1
+                name: 'شاي أحمر',
+                image: redTea,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
             },
             {
                 id: 4,
-                name: 'شوكو',
-                image: choco,
-                isAvailable: 1
+                name: 'شاي أخضر',
+                image: greenTea,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
             },
             {
                 id: 5,
+                name: 'توفي لاتيه',
+                image: cappuccino,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 6,
+                name: 'شوكو',
+                image: choco,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 7,
+                name: 'شوكو بالحليب',
+                image: choco,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 8,
+                name: 'حليب',
+                image: milk,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 9,
+                name: 'سحلب',
+                image: milk,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 10,
                 name: 'كابتشينو',
                 image: cappuccino,
-                isAvailable: 1
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 11,
+                name: 'يانسون',
+                image: herbal,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 12,
+                name: 'قرفة',
+                image: herbal,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 13,
+                name: 'أعشاب',
+                image: herbal,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
+            },
+            {
+                id: 14,
+                name: 'بابونج',
+                image: herbal,
+                isAvailable: 1,
+                prices: {
+                    m: 2,
+                    l: 3
+                }
             }
         ]
     }
